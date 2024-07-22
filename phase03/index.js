1c130c639816d3d3307bd2183290d88af3520879
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-07-22 13:46:08 +0330

    add styles to page

--- phase03/index.js
+++ phase03/index.js
@@ -23,17 +23,17 @@ fetchData().then(() => {
 
     for (const book of books) {
         const bookElement = document.createElement('div');
+        bookElement.setAttribute('class', 'book-card');
         bookElement.innerHTML = `
-            <h3>${book.name}</h3>
-            <p>
-                Genre: ${book.genre.join(', ')}
-                <br/>
-                Author: ${book.author}
-                <br/>
-                Publish Date: ${book.publishData}
-                <br/>
-                Price: ${book.price}$
-            </p>
+            <div class="description">
+                <h3>${book.name}</h3>
+                <p>
+                    <span><span class="title" >Genre</span>${book.genre.join(', ')}</span>
+                    <span><span class="title" >Author</span>${book.author}</span>
+                    <span><span class="title" >Publish Date</span>${book.publishData}</span>
+                    <span><span class="title" >Price</span>${book.price}$</span>
+                </p>
+            </div>
             <img src="${book.image}" alt="${book.name}" />
         `;
         booksContainer.appendChild(bookElement);
