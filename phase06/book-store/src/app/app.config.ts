309a46706f7560aacf3e8d023667fadfaed4d20a
ArashAzarpoor <azarpoor81@gmail.com>
2024-07-31 13:20:07 +0330

    Add route transition

--- phase06/book-store/src/app/app.config.ts
+++ phase06/book-store/src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router';
+import routeConfig from "./routes";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routeConfig,
+      withViewTransitions(),
+      withComponentInputBinding()
+    )
+  ]
 };
